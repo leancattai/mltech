ccadc03d18da693c82a3088f62e8b7310f40cb20
Marian <marianoorozco700@outlook.com.ar>
2025-11-15 16:07:05 -0300

    Implementacion de envios de consultas por wsp con TWILIO

--- backend/routes/contacto.py
+++ backend/routes/contacto.py
@@ -1,5 +1,7 @@
+import os
 from flask import Blueprint, flash, redirect, request, render_template, url_for
 from ..models import Contact
+from ..utils.send_message import send_message_whatsapp
 from ..config.config_db import db
 
 contacto_bp = Blueprint("contacto", __name__)
@@ -14,16 +16,27 @@ def contacto():
         try:
 
             data = request.form
-            print("data:", data)
             new_contact = Contact.from_json(data)
             db.session.add(new_contact)
             db.session.commit()
 
         except Exception as e:
             print(e)
-            flash("Error al enviar el mensaje", "error")
+            flash("Error al guardar la informacion en base de datos", "error")
 
-                
+        try:
+
+            message = f""" Nuevo mensaje de contacto M&L Tech\n{data.get('name')} con email: {data.get('email')}, telefono: {data.get('telefono')}, quiere consultar sobre {data.get('area_interes')} con presupuesto de {data.get('presupuesto')} y dejo el siguiente mensaje: \n{data.get('message')}\n\nPara o rechazar este pedido ingresar al apartado de back office y darle a Aceptar o Rechazar."""
+   
+            phone_number_mariano = os.getenv('PHONE_NUMBER_MARIANO')
+            phone_number_leandro = os.getenv('PHONE_NUMBER_LEANDRO')
+
+            send_message_whatsapp(phone_number_mariano, message)
+            send_message_whatsapp(phone_number_leandro, message)
+
+        except Exception as e:
+            print(e)
+            flash("Error al enviar el mensaje por WhatsApp", "error")    
         
 
     
